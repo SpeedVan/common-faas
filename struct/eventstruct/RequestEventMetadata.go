fbe316fb160885c81b8ef450643d59bd5e95025f
admin <admin@localhost.local>
2020-02-12 14:28:32 +0800

    convert method

--- struct/eventstruct/RequestEventMetadata.go
+++ struct/eventstruct/RequestEventMetadata.go
@@ -1,6 +1,12 @@
 package eventstruct
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+
+	"github.com/SpeedVan/go-common-faas/constant/httpconst"
+	"github.com/SpeedVan/invoker/service"
+)
 
 // RequestEventMetadata todo
 type RequestEventMetadata struct {
@@ -9,4 +15,30 @@ type RequestEventMetadata struct {
 	Method             string                 `json:"method"`
 	Path               string                 `json:"path"`
 	Header             http.Header            `json:"header"`
+}
+
+// FormHTTPRequest todo
+func FormHTTPRequest(r *http.Request, responseStreamName string) *RequestEventMetadata {
+	header := r.Header
+
+	ctx := make(map[string]interface{})
+
+	for k, item := range header {
+		if strings.HasPrefix(k, httpconst.HeaderPrefix) {
+			if len(item) > 0 {
+				ctx[k] = item[0]
+			} else {
+				ctx[k] = ""
+			}
+			header.Del(k)
+		}
+	}
+
+	return &service.RequestEventMetadata{
+		ResponseStreamName: responseStreamName,
+		Context:            ctx,
+		Method:             r.Method,
+		Path:               eventPath,
+		Header:             header,
+	}
 }
