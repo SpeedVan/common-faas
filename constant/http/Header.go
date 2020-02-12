17319ba6b190e26eb51f9be391b885b47ab0c2fe
admin <admin@localhost.local>
2020-02-12 13:17:45 +0800

    callback mod constant

--- constant/http/Header.go
+++ constant/http/Header.go
@@ -7,4 +7,8 @@ var (
 	TraceID = "X-Faas-Trace-Id"
 	// RequestID todo
 	RequestID = "X-Faas-Request-Id"
+	// CallbackMethod todo
+	CallbackMethod = "X-Faas-Callback-Method"
+	// CallbackURL todo
+	CallbackURL = "X-Faas-Callback-Url"
 )
